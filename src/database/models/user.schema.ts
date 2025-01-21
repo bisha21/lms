1836d2378577a4a5585e83d844ce9f79c30fa34e
[bisha21] <[timilsinab22bi@gmail.com]>
2025-01-21 20:11:06 +0545

    `Added fields to user schema and created User model`

--- src/database/models/user.schema.ts
+++ src/database/models/user.schema.ts
@@ -1,5 +1,15 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 const userSchema= new Schema({
-    
+    username:String,
+    email:String,
+    googleId:{
+        type:String,
+    },
+    profileImage:{
+        type:String,
+    }
+
 })
+const User= mongoose.model("User",userSchema);
+export default User;
