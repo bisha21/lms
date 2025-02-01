e239e276cf7bdd5cc28cff6a13a4c5d9d9df17b8
[bisha21] <[timilsinab22bi@gmail.com]>
2025-02-01 21:20:14 +0545

    `feat: updated next-auth configuration and user schema for Google authentication`

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,35 @@
-import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
+import { createConnection } from '@/database/db';
+import User from '@/database/models/user.schema';
+import NextAuth from 'next-auth/next';
+import GoogleProvider from 'next-auth/providers/google';
 
 const handler = NextAuth({
-    providers:[
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-        },
-)
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.GOOGLE_NEXT_AUTH_SECRET,
+  callbacks: {
+    async signIn({ user }): Promise<boolean> {
+      try {
+        createConnection();
+        const existingUser = await User.findOne({ email: user.email });
+        if (!existingUser) {
+          await User.create({
+            email: user.email,
+            username: user.name,
+            profileImage: user.image,
+          });
+        }
+        return true;
+      } catch (err) {
+        console.log(err);
+        return false;
+      }
+    },
+  },
+});
 
-    ],
-    secret: process.env.GOOGLE_CLIENT_SECRET
-})
-
-export {handler as GET , handler as POST}
+export { handler as GET, handler as POST };
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,23 @@
-"use client"
-import {signIn} from "next-auth/react"
-const Page = () => {
-  return (
-    <div className='w-[100vw] h-[100vh] flex items-center justify-center'>
-        <button className='bg-green-500 hover:bg-grenn-700 text-white font-bold py-2 px-4 rounded-md shadow-md'
-        onClick={()=>signIn("google")}
-        >Continue with google</button>
-    </div>
-  )
-}
+'use client';
+import { useSession, signIn, signOut } from 'next-auth/react';
+import Image from 'next/image';
 
-export default Page
+export default function Home() {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        Welcome  {session.user?.name} to our Lms<br />
+        <p>your email: {session.user?.email}</p>
+        <Image src={session.user?.image || '/vercel.svg'} alt="Vercel Logo" width={100} height={24} />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  );
+}
